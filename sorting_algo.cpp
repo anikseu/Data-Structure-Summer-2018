7f072369fbb69be7349dbc3b8f4d429799554b17
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-06-07 22:18:25 +0600

    Sorting Algo

---
+++ sorting_algo.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int a[], int n){
+	
+	for(int j=0; j<=n-2; j++){
+		for(int i=0; i<=n-2-j; i++){
+			if(a[i]>a[i+1])
+			swap(a[i], a[i+1]); 
+		}
+	}
+}
+
+void insertion_sort(int a[], int n){
+	int key, j;
+	for(int i=1; i<=n-1; i++){
+		key=a[i]; 
+		j = i-1; 
+		while(a[j]>key && j>=0){
+			a[j+1]=a[j]; 
+			j=j-1; 
+		}
+		a[j+1]=key; 
+	}
+}
+
+void printArray(int a[], int n){
+	
+	for(int i=0; i<n; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl; 
+	
+}
+
+int main(){
+
+	int a[]={5,4,3,2,1};
+	int n=5;
+	
+	bubble_sort(a,n); 
+	//insertion_sort(a, n); 
+	printArray(a, n); 
+	
+}
