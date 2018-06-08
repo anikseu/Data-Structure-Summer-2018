ab2d530de7de03f791612b1ff3a2e3429cb5e820
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-06-08 11:20:29 +0600

    Small fix

--- sorting_algo.cpp
+++ sorting_algo.cpp
@@ -12,10 +12,10 @@ void bubble_sort(int a[], int n){
 }
 
 void insertion_sort(int a[], int n){
-	int key, j;
+	
 	for(int i=1; i<=n-1; i++){
-		key=a[i]; 
-		j = i-1; 
+		int key=a[i]; 
+		int j = i-1; 
 		while(a[j]>key && j>=0){
 			a[j+1]=a[j]; 
 			j=j-1; 
