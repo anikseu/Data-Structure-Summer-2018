c1c258fa526a9f904a1fcd1c3cd09d159a8ba130
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-07-02 14:09:22 +0600

    Fixed Formatting

--- binary_search.cpp
+++ binary_search.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int binary_search(int arr[], int l, int r, int x){
 	
-	int mid = (l+r)/2; 
+    int mid = (l+r)/2; 
 	
-	if (arr[mid] == x)
+    if (arr[mid] == x)
     {
         return mid; 
     }
@@ -18,26 +18,26 @@ int binary_search(int arr[], int l, int r, int x){
         binary_search(arr, mid + 1, r, x);
     }
     else
-	{
+    {
     return -1;	
-	}
+    }
     
 	
 }
 
 int main(){
   	
-  	int arr[] = {1,2,3,4,5}; 
-  	int r = 4; // n-1, ending index 
-  	int l=0; // starting index
+    int arr[] = {1,2,3,4,5}; 
+    int r = 4; // n-1, ending index 
+    int l=0; // starting index
   
     int query=5; // Searching for the Number '5' 
-  	int res=binary_search(arr,l,r,query); // must return index num= 4 
+    int res=binary_search(arr,l,r,query); // must return index num= 4 
     
     
     if(res==-1)
     cout << "Not Found!" << endl; 
     else
-  	cout << res << endl; 
+    cout << res << endl; 
   	
 }
